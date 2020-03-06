ed484a0442e1dfdde84034e6ca1106f75470702d
Sam <samuel_morgan_@outlook.com>
2020-03-06 14:37:41 +0000

    Updated start script

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,10 @@ import Table from './Table';
 import taskList from '../data/taskList';
 
 const App = () => {
+	//set state to determine what mode table is in
+
+	//sort tasklist according to state
+
 	return (
 		<div>
 			<h1>PROJECT MANAGEMENT PLATFORM</h1>
