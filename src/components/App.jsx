829d94021d9b4038df65ea6239d6bfdd48e32310
Sam <samuel_morgan_@outlook.com>
2020-03-06 14:45:34 +0000

    Created sort list file structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Table from './Table';
 import taskList from '../data/taskList';
+import sortList from '../processing/sortList';
 
 const App = () => {
+	const [ tableView, setTableView ] = useState('tasks');
+	const [ tableSort, setTableSort ] = useState('date');
 	//set state to determine what mode table is in
 
 	//sort tasklist according to state
+	sortList();
 
 	return (
 		<div>
---
+++ src/processing/sortList.js
@@ -0,0 +1,5 @@
+export const sortList = () => {
+	console.log('sorted data');
+};
+
+export default sortList;
