186ab9bcbb8a609f50ae05cfa3c0b709bd9b4958
Sam <samuel_morgan_@outlook.com>
2020-03-06 22:33:53 +0000

    Fixed bug where clicking non heading cell crashed

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cell = (props) => {
 	return (
-		<div className="cell" onClick={() => props.onClick()}>
+		<div className="cell" onClick={() => (props.onClick ? props.onClick() : null)}>
 			<div>{props.text === true ? '✓' : props.text} </div>
 		</div>
 	);
