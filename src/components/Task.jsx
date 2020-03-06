c3a78d8895e190108df4c1402bc0a8c295192dfd
Sam <samuel_morgan_@outlook.com>
2020-03-06 22:02:31 +0000

    parseISOToDDMMYYYY()

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -5,13 +5,18 @@ const Task = (props) => {
 	return (
 		<div className="taskEntry">
 			<Cell text={props.task} />
-			<Cell text={new Date(props.date).toString()} />
-			<Cell text={new Date(props.deadline).toString()} />
+			<Cell text={parseISOToDDMMYYYY(props.date)} />
+			<Cell text={parseISOToDDMMYYYY(props.deadline)} />
 			<Cell text={props.urgency} />
 			<Cell text={props.team} />
 			<Cell text={props.completed} />
 		</div>
 	);
+};
+
+const parseISOToDDMMYYYY = (string) => {
+	const stringDate = string.match(/.+?(?=T)/g)[0];
+	return stringDate.split('-').reverse().join('/');
 };
 
 export default Task;
--- src/data/taskList.js
+++ src/data/taskList.js
@@ -1,35 +1,32 @@
-var d = new Date('2020-01-01T00:00:00');
-console.log(d.toString());
-
 const taskList = [
 	{
 		task: 'Create App',
-		date: '2020-01-01T00:00:00',
-		deadline: '2020-01-01T00:00:00',
+		date: '2020-01-01T00:00:00.000Z',
+		deadline: '2020-01-01T00:00:00.000Z',
 		urgency: 3,
 		team: 'Team 1',
 		completed: false
 	},
 	{
 		task: 'Add Payment Feature',
-		date: '2020-01-01T00:00:00',
-		deadline: '2020-01-01T00:00:00',
+		date: '2020-03-01T00:00:00.000Z',
+		deadline: '2020-06-01T00:00:00.000Z',
 		urgency: 4,
 		team: 'Team 3',
 		completed: true
 	},
 	{
 		task: 'Refactor Backend',
-		date: '2020-01-01T00:00:00',
-		deadline: '2020-01-01T00:00:00',
+		date: '2020-01-01T00:00:00.000Z',
+		deadline: '2020-01-01T00:00:00.000Z',
 		urgency: 9,
 		team: 'Team 2',
 		completed: false
 	},
 	{
 		task: 'Redesign Homepage',
-		date: '2020-01-01T00:00:00',
-		deadline: '2020-01-01T00:00:00',
+		date: '2020-01-01T00:00:00.000Z',
+		deadline: '2020-01-01T00:00:00.000Z',
 		urgency: 2,
 		team: 'Team 2',
 		completed: false
