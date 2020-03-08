24b75cc574f98e382ca1b7eb31ea6ec932919708
Sam <samuel_morgan_@outlook.com>
2020-03-08 21:08:38 +0000

    Fixed dropdown key warning

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,55 +1,51 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-const DateSelect = props => {
-  const [listOpen, setListOpen] = useState(false);
-  const [defaultValue, setDefaultValue] = useState(props.placeholder);
-  const [hoveredOption, setHoveredOption] = useState(null);
+const DateSelect = (props) => {
+	const [ listOpen, setListOpen ] = useState(false);
+	const [ defaultValue, setDefaultValue ] = useState(props.placeholder);
+	const [ hoveredOption, setHoveredOption ] = useState(null);
 
-  const dropdownStyle = {
-    height: "2rem",
-    width: "5rem",
-    border: "1px solid black",
-    backgroundColor: "#ccc",
-    zIndex: "2"
-  };
+	const dropdownStyle = {
+		height: '2rem',
+		width: '5rem',
+		border: '1px solid black',
+		backgroundColor: '#ccc',
+		zIndex: '2'
+	};
 
-  const optionDivs = props.options.map(el => (
-    <div
-      className="dropdownOption"
-      style={{ ...dropdownStyle, position: "relative" }}
-    ></div>
-  ));
+	const optionDivs = props.options.map((el, i) => (
+		<div key={i} className="dropdownOption" style={{ ...dropdownStyle, position: 'relative' }} />
+	));
 
-  useEffect(() => {
-    if (listOpen) {
-      document.addEventListener("mousedown", whileDropdownOpenClick);
-    } else {
-      document.removeEventListener("mousedown", whileDropdownOpenClick);
-    }
-    return () =>
-      document.removeEventListener("mousedown", whileDropdownOpenClick);
-  });
+	useEffect(() => {
+		if (listOpen) {
+			document.addEventListener('mousedown', whileDropdownOpenClick);
+		} else {
+			document.removeEventListener('mousedown', whileDropdownOpenClick);
+		}
+		return () => document.removeEventListener('mousedown', whileDropdownOpenClick);
+	});
 
-  const whileDropdownOpenClick = e => {
-    if (e.target.className === "dropdown") {
-      return;
-    }
-    setListOpen(false);
-  };
+	const whileDropdownOpenClick = (e) => {
+		if (e.target.className === 'dropdown') {
+			return;
+		}
+		setListOpen(false);
+	};
 
-  return (
-    <div className="dropdown">
-      <div
-        className="dropdownHeader"
-        style={{ ...dropdownStyle, position: "relative" }}
-        onMouseDown={e => {
-          if (e.buttons === 1) setListOpen(!listOpen);
-        }}
-        onContextMenu={e => e.preventDefault()}
-      ></div>
-      {listOpen ? optionDivs : null}
-    </div>
-  );
+	return (
+		<div className="dropdown">
+			<div
+				className="dropdownHeader"
+				style={{ ...dropdownStyle, position: 'relative' }}
+				onMouseDown={(e) => {
+					if (e.buttons === 1) setListOpen(!listOpen);
+				}}
+				onContextMenu={(e) => e.preventDefault()}
+			/>
+			{listOpen ? optionDivs : null}
+		</div>
+	);
 };
 
 export default DateSelect;
