30d5dd4826b877598b5c1995cf5ea5fdf5660087
Sam <samuel_morgan_@outlook.com>
2020-03-08 21:11:32 +0000

    Fixed table z index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,35 @@
-import React, { useEffect, useState } from "react";
-import Table from "./Table";
-import FilterBar from "./FilterBar";
-import taskList from "../data/taskList";
-import sortList from "../processing/sortList";
+import React, { useEffect, useState } from 'react';
+import Table from './Table';
+import FilterBar from './FilterBar';
+import taskList from '../data/taskList';
+import sortList from '../processing/sortList';
 
 const App = () => {
-  // const [ tableView, setTableView ] = useState('tasks');
+	// const [ tableView, setTableView ] = useState('tasks');
 
-  const [sortOptions, setSortOptions] = useState({
-    type: "date",
-    reversed: false
-  });
+	const [ sortOptions, setSortOptions ] = useState({
+		type: 'date',
+		reversed: false
+	});
 
-  function userSetSort(sort) {
-    if (sort === sortOptions.type) {
-      setSortOptions({ ...sortOptions, reversed: !sortOptions.reversed });
-    } else {
-      setSortOptions({
-        type: sort,
-        reversed: false
-      });
-    }
-  }
-
-  const sortedList = sortList(sortOptions, taskList);
-
-  return (
-    <div className="mainPage">
-      <h1>PROJECT MANAGEMENT PLATFORM</h1>
-      <FilterBar />
-      <Table
-        taskList={sortedList}
-        sortOptions={sortOptions}
-        userSetSort={val => userSetSort(val)}
-      />
-    </div>
-  );
+	function userSetSort(sort) {
+		if (sort === sortOptions.type) {
+			setSortOptions({ ...sortOptions, reversed: !sortOptions.reversed });
+		} else {
+			setSortOptions({
+				type: sort,
+				reversed: false
+			});
+		}
+	}
+	const sortedList = sortList(sortOptions, taskList);
+	return (
+		<div className="mainPage">
+			<h1>PROJECT MANAGEMENT PLATFORM</h1>
+			<FilterBar />
+			<Table taskList={sortedList} sortOptions={sortOptions} userSetSort={(val) => userSetSort(val)} />
+		</div>
+	);
 };
 
 export default App;
