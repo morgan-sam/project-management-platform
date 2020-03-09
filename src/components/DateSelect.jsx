1f8c31ae2643d1bb9e4bb61492c891334ae10b8b
Sam <samuel_morgan_@outlook.com>
2020-03-09 10:33:23 +0000

    Can select date & year in dropdown

--- src/components/DateSelect.jsx
+++ src/components/DateSelect.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Dropdown from './Dropdown';
 import { months, daysInMonth, get21stCenturyYears } from '../data/dates';
 
 const DateSelect = (props) => {
-	console.log(months());
-	console.log(get21stCenturyYears());
-	console.log(daysInMonth(3, 2020));
+	const [ selectedMonth, setSelectedMonth ] = useState('January');
+	const [ selectedYear, setSelectedYear ] = useState(2020);
 
+	console.log(selectedMonth);
+	console.log(selectedYear);
 	return (
 		<div className="dateSelect" style={{ display: 'flex' }}>
 			<Dropdown onClick={(val) => console.log(`${val} selected.`)} />
-			<Dropdown onClick={(val) => console.log(`${val} selected.`)} />
-			<Dropdown onClick={(val) => console.log(`${val} selected.`)} />
+			<Dropdown default={selectedMonth} options={months()} onClick={(val) => setSelectedMonth(val)} />
+			<Dropdown default={selectedYear} options={get21stCenturyYears()} onClick={(val) => setSelectedYear(val)} />
 		</div>
 	);
 };
