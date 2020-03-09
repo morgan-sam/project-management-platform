8f4f34ec52c8474d0e13fe6ed47eea31777f157f
Sam <samuel_morgan_@outlook.com>
2020-03-09 12:35:34 +0000

    Dropdown shows correct values

--- src/components/DateSelect.jsx
+++ src/components/DateSelect.jsx
@@ -3,15 +3,17 @@ import Dropdown from './Dropdown';
 import { months, arrayOfMonthDays, get21stCenturyYears } from '../data/dates';
 
 const DateSelect = (props) => {
-	const [ selectedMonth, setSelectedMonth ] = useState('February');
+	const [ selectedMonth, setSelectedMonth ] = useState('January');
 	const [ selectedYear, setSelectedYear ] = useState(2020);
+	const [ selectedDay, setSelectedDay ] = useState(1);
 
-	console.log(arrayOfMonthDays(selectedMonth, selectedYear));
-	console.log(selectedMonth);
-	console.log(selectedYear);
 	return (
 		<div className="dateSelect" style={{ display: 'flex' }}>
-			<Dropdown onClick={(val) => console.log(`${val} selected.`)} />
+			<Dropdown
+				default={selectedDay}
+				options={arrayOfMonthDays(selectedMonth, selectedYear)}
+				onClick={(val) => setSelectedDay(val)}
+			/>
 			<Dropdown default={selectedMonth} options={months()} onClick={(val) => setSelectedMonth(val)} />
 			<Dropdown default={selectedYear} options={get21stCenturyYears()} onClick={(val) => setSelectedYear(val)} />
 		</div>
