e8972aa7b324057d1d4958b7f236992a17df0912
Sam <samuel_morgan_@outlook.com>
2020-03-10 09:33:05 +0000

    Fixed positioning of filter labels

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import DateSelect from './DateSelect';
 
 const FilterBar = (props) => {
-	const filterBarStyle = { display: 'flex', flexDirection: 'row' };
+	const filterBarStyle = {
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center'
+	};
 
 	return (
 		<div className="filterBar" style={{ ...filterBarStyle, ...props.style }}>
+			<div>Start Date:</div>
 			<DateSelect />
+			<div>End Date:</div>
 			<DateSelect />
 		</div>
 	);
