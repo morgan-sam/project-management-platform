2e68e18d615ddfb8f36f28e4eaa4d710c20112ec
Sam <samuel_morgan_@outlook.com>
2020-03-10 09:48:06 +0000

    Table headings unselectable

--- src/components/TableHeadings.jsx
+++ src/components/TableHeadings.jsx
@@ -1,43 +1,37 @@
-import React from "react";
-import Cell from "./Cell";
+import React from 'react';
+import Cell from './Cell';
 
-const TableHeadings = props => {
-  const sortArrow = props.sortOptions.reversed ? "↓" : "↑";
+const TableHeadings = (props) => {
+	const sortArrow = props.sortOptions.reversed ? '↓' : '↑';
 
-  return (
-    <div className="tableHeadings" style={{ cursor: "pointer" }}>
-      <Cell
-        text={`Task ${props.sortOptions.type === "task" ? sortArrow : ""}`}
-        onClick={() => props.userSetSort("task")}
-      />
-      <Cell
-        text={`Date ${props.sortOptions.type === "date" ? sortArrow : ""}`}
-        onClick={() => props.userSetSort("date")}
-      />
-      <Cell
-        text={`Deadline ${
-          props.sortOptions.type === "deadline" ? sortArrow : ""
-        }`}
-        onClick={() => props.userSetSort("deadline")}
-      />
-      <Cell
-        text={`Urgency ${
-          props.sortOptions.type === "urgency" ? sortArrow : ""
-        }`}
-        onClick={() => props.userSetSort("urgency")}
-      />
-      <Cell
-        text={`Team ${props.sortOptions.type === "team" ? sortArrow : ""}`}
-        onClick={() => props.userSetSort("team")}
-      />
-      <Cell
-        text={`Completed ${
-          props.sortOptions.type === "completed" ? sortArrow : ""
-        }`}
-        onClick={() => props.userSetSort("completed")}
-      />
-    </div>
-  );
+	return (
+		<div className="tableHeadings" style={{ cursor: 'pointer', userSelect: 'none' }}>
+			<Cell
+				text={`Task ${props.sortOptions.type === 'task' ? sortArrow : ''}`}
+				onClick={() => props.userSetSort('task')}
+			/>
+			<Cell
+				text={`Date ${props.sortOptions.type === 'date' ? sortArrow : ''}`}
+				onClick={() => props.userSetSort('date')}
+			/>
+			<Cell
+				text={`Deadline ${props.sortOptions.type === 'deadline' ? sortArrow : ''}`}
+				onClick={() => props.userSetSort('deadline')}
+			/>
+			<Cell
+				text={`Urgency ${props.sortOptions.type === 'urgency' ? sortArrow : ''}`}
+				onClick={() => props.userSetSort('urgency')}
+			/>
+			<Cell
+				text={`Team ${props.sortOptions.type === 'team' ? sortArrow : ''}`}
+				onClick={() => props.userSetSort('team')}
+			/>
+			<Cell
+				text={`Completed ${props.sortOptions.type === 'completed' ? sortArrow : ''}`}
+				onClick={() => props.userSetSort('completed')}
+			/>
+		</div>
+	);
 };
 
 export default TableHeadings;
