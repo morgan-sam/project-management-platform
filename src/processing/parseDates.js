872c75c17259c1ceef98987911366bf21993c157
Sam <samuel_morgan_@outlook.com>
2020-03-10 10:15:03 +0000

    Created parseDateObjToISO Fn

--- src/processing/parseDates.js
+++ src/processing/parseDates.js
@@ -2,3 +2,9 @@ export const parseISOToLittleEndian = (string) => {
 	const stringDate = string.match(/.+?(?=T)/g)[0];
 	return stringDate.split('-').reverse().join('/');
 };
+
+export const parseDateObjToISO = (obj) => {
+	const dayString = obj.day >= 10 ? obj.day.toString() : '0' + obj.day.toString();
+	const monthString = obj.month >= 10 ? obj.month.toString() : '0' + obj.month.toString();
+	return `${obj.year}-${monthString}-${dayString}T00:00:00.000Z`;
+};
