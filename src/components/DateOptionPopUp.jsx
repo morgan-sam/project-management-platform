954a1d60b4f83c5f63beb3f3d2567d3139b8c0d8
Sam <samuel_morgan_@outlook.com>
2020-03-11 16:36:24 +0000

    Skeleton date confirm cancel btns

--- src/components/DateOptionPopUp.jsx
+++ src/components/DateOptionPopUp.jsx
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import DateSelect from './DateSelect';
 import { parseDateObjToISO } from '../processing/parseDates';
-import { dateOptionPopUpStyling, dateDisplayBoxStyling, datePopUpStyling } from '../styling/dateOption';
+import {
+	dateOptionPopUpStyling,
+	dateDisplayBoxStyling,
+	datePopUpStyling,
+	canConBtnStyle,
+	confirmBtnStyle,
+	cancelBtnStyle,
+	canConContainerStyle
+} from '../styling/dateOption';
 
 const DateOptionPopUp = (props) => {
 	const [ selectedDate, setSelectedDate ] = useState('test');
@@ -18,14 +26,25 @@ const DateOptionPopUp = (props) => {
 			<DateSelect
 				style={{
 					...datePopUpStyling,
-					display: showDateSelect ? 'grid' : 'none',
-					border: '1px solid black'
+					display: showDateSelect ? 'grid' : 'none'
 				}}
 				setDate={(date) => {
 					setSelectedDate(date);
 					props.setFilterDate(parseDateObjToISO(date));
 				}}
 			/>
+
+			<div
+				className="canConBtnContainer"
+				style={{ ...canConContainerStyle, display: showDateSelect ? 'flex' : 'none' }}
+			>
+				<button className="cancelButton" style={{ ...canConBtnStyle, ...confirmBtnStyle }}>
+					X
+				</button>
+				<button className="confirmButton" style={{ ...canConBtnStyle, ...cancelBtnStyle }}>
+					✓
+				</button>
+			</div>
 		</div>
 	);
 };
--- src/styling/dateOption.js
+++ src/styling/dateOption.js
@@ -1,12 +1,11 @@
 export const dateOptionPopUpStyling = {
-	//
+	border: '1px solid black'
 };
 
 export const dateDisplayBoxStyling = {
 	border: '1px solid black',
 	padding: '0 0.5rem',
 	textAlign: 'center',
-
 	display: 'flex',
 	alignItems: 'center',
 	justifyContent: 'center'
@@ -15,4 +14,26 @@ export const dateDisplayBoxStyling = {
 export const datePopUpStyling = {
 	border: '1px solid lblack',
 	textAlign: 'center'
+};
+
+export const canConBtnStyle = {
+	margin: '0.5rem',
+	height: '2rem',
+	width: '2rem',
+	justifyContent: 'center'
+};
+
+export const canConContainerStyle = {
+	display: 'flex',
+	flexDirection: 'row',
+	alignItems: 'center',
+	paddingRight: '0.75rem'
+};
+
+export const confirmBtnStyle = {
+	backgroundColor: '#fb9898'
+};
+
+export const cancelBtnStyle = {
+	backgroundColor: '#c1fcc1'
 };
