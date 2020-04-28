df7b4eaa0d58a6bba37156116c3cd9c262ea6777
Sam <samuel_morgan_@outlook.com>
2020-04-28 18:13:09 +0100

    Improve off screen slide animation

--- client/src/styling/dateOptionPopUp.js
+++ client/src/styling/dateOptionPopUp.js
@@ -12,7 +12,7 @@ export const displayOffscreen = {
 	position: 'absolute',
 	left: '0%',
 	top: '50%',
-	transform: 'translate(-50%,-50%)',
+	transform: 'translate(-150%,-50%)',
 	zIndex: '5',
 	opacity: '0'
 };
@@ -30,7 +30,7 @@ export const selectOffscreen = {
 	position: 'absolute',
 	left: '0%',
 	top: '50%',
-	transform: 'translate(50%,-50%)',
+	transform: 'translate(100%,-50%)',
 	zIndex: '3',
 	opacity: '0'
 };
