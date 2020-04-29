737ef7fa649f75fb10ab39326febf1e25924ae4d
Sam <samuel_morgan_@outlook.com>
2020-04-29 14:17:18 +0100

    Non active form spacing

--- client/src/components/TaskNameInput.jsx
+++ client/src/components/TaskNameInput.jsx
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
 const TaskNameInput = (props) => {
-	return <div>{'TaskNameInput'}</div>;
+	const taskNameInputStyle = {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: '0 1rem'
+	};
+
+	const formItemsStyle = {
+		margin: '0 0.5rem'
+	};
+
+	return (
+		<div style={{ ...props.style, ...taskNameInputStyle }}>
+			<form>
+				<label style={formItemsStyle}>Task Name:</label>
+				<input style={formItemsStyle} type="text" name="name" />
+				<input type="submit" value="Submit" style={{ display: 'none' }} />
+			</form>
+		</div>
+	);
 };
 
 export default TaskNameInput;
