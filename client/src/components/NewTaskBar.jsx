6914044255f50a4fd699f932eb6c762e760c4c40
Sam <samuel_morgan_@outlook.com>
2020-05-01 18:12:59 +0100

    getTaskBar[Hidden/Visible]Style fns sep file

--- client/src/components/NewTaskBar.jsx
+++ client/src/components/NewTaskBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { newTaskBarStyle } from 'styling/newTaskBar';
+import { newTaskBarStyle, getTaskBarHiddenStyle, getTaskBarVisibleStyle } from 'styling/newTaskBar';
 import TaskNameInput from './TaskNameInput';
 import DateRangeSelect from 'components/DateRangeSelect';
 import { parseISOToDateObj } from 'processing/parseDates';
@@ -8,29 +8,6 @@ import DropdownWithLabel from 'components/DropdownWithLabel';
 const NewTaskBar = (props) => {
 	const [ overflowHidden, setOverflowHidden ] = useState(true);
 	const [ popUpOpen, setPopUpOpen ] = useState(false);
-
-	const smallOpenTransition = 'max-height 1s cubic-bezier(.23,.52,.53,.74)';
-	const smallCloseTransition = 'max-height .8s cubic-bezier(.27,.97,.36,.96)';
-
-	const largeCloseTransition = 'max-height 1s cubic-bezier(.41,.49,.23,.93)';
-	const largeOpenTransition = 'max-height 0.7s cubic-bezier(.38,.03,.23,.93)';
-
-	const taskBarHidden = {
-		opacity: '0',
-		maxHeight: '0',
-		zIndex: '-10',
-		//executes on task bar close
-		transition: `${popUpOpen
-			? largeCloseTransition
-			: smallCloseTransition}, opacity 1s cubic-bezier(0,1.06,.62,.99)`
-	};
-
-	const taskBarVisible = {
-		maxHeight: '11rem',
-		opacity: '1',
-		//executes on task bar open
-		transition: `${popUpOpen ? largeOpenTransition : smallOpenTransition}, opacity 1s`
-	};
 
 	const [ date, setDate ] = useState('2013-03-10T02:00:00Z');
 	const [ deadline, setDeadline ] = useState('2013-03-10T02:00:00Z');
@@ -43,7 +20,7 @@ const NewTaskBar = (props) => {
 				style={{
 					...props.style,
 					...newTaskBarStyle,
-					...(props.displayNewTaskBar ? taskBarHidden : taskBarVisible),
+					...(props.displayNewTaskBar ? getTaskBarHiddenStyle(popUpOpen) : getTaskBarVisibleStyle(popUpOpen)),
 					overflow: overflowHidden ? 'visible' : 'hidden'
 				}}
 			>
--- client/src/styling/newTaskBar.js
+++ client/src/styling/newTaskBar.js
@@ -9,3 +9,30 @@ export const newTaskBarStyle = {
 	overflow: 'hidden',
 	zIndex: '8'
 };
+
+const smallOpenTransition = 'max-height 1s cubic-bezier(.23,.52,.53,.74)';
+const smallCloseTransition = 'max-height .8s cubic-bezier(.27,.97,.36,.96)';
+
+const largeCloseTransition = 'max-height 1s cubic-bezier(.41,.49,.23,.93)';
+const largeOpenTransition = 'max-height 0.7s cubic-bezier(.38,.03,.23,.93)';
+
+export const getTaskBarHiddenStyle = (popUpOpen) => {
+	return {
+		opacity: '0',
+		maxHeight: '0',
+		zIndex: '-10',
+		//executes on task bar close
+		transition: `${popUpOpen
+			? largeCloseTransition
+			: smallCloseTransition}, opacity 1s cubic-bezier(0,1.06,.62,.99)`
+	};
+};
+
+export const getTaskBarVisibleStyle = (popUpOpen) => {
+	return {
+		maxHeight: '11rem',
+		opacity: '1',
+		//executes on task bar open
+		transition: `${popUpOpen ? largeOpenTransition : smallOpenTransition}, opacity 1s`
+	};
+};
