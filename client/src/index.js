4d9c776b4194db0e00f31759eccfd52987c7d333
Sam <samuel_morgan_@outlook.com>
2020-05-02 20:46:39 +0100

    Basic quick btn color transition

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'css/reset.css';
 import 'css/index.css';
+import 'css/animations.css';
 import App from 'components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
