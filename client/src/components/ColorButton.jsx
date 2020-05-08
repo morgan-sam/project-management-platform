c69cea0717ec5b8279866a5136ad77923871403e
Sam <samuel_morgan_@outlook.com>
2020-05-08 11:47:09 +0100

    colorButton style sep file

--- client/src/components/ColorButton.jsx
+++ client/src/components/ColorButton.jsx
@@ -1,77 +1,27 @@
 import React, { useState, useRef } from 'react';
+import {
+	BUTTON_BOUNCE_PX,
+	getStaticContainerStyle,
+	getFloatingContainerStyle,
+	getButtonStyle,
+	getColorBoxStyle,
+	whiteBoxStyle
+} from 'styling/colorButton';
 
 const ColorButton = (props) => {
-	const BUTTON_BOUNCE_PX = 8;
-
 	const [ hover, setHover ] = useState(false);
 	const [ pressed, setPressed ] = useState(false);
 	const floatRef = useRef();
 
-	const btnWidth = props.style && props.style.width ? props.style.width : 'auto';
-	const btnHeight = props.style && props.style.height ? props.style.height : 'auto';
-
-	const staticContainerStyle = {
-		position: 'relative',
-		height: props.style && props.style.height ? `${parseInt(props.style.height) * 1.8}rem` : 'auto',
-		display: 'flex',
-		alignItems: 'center',
-		margin: '0.5rem'
-	};
-
-	const floatingContainerStyle = {
-		position: 'relative',
-		width: btnWidth,
-		height: btnHeight,
-		top: pressed ? `${BUTTON_BOUNCE_PX}px` : hover ? `-${BUTTON_BOUNCE_PX}px` : '0',
-		transition: 'top 0.2s ease-in-out',
-		animation: hover ? 'float 1.4s ease-in-out 0.4s alternate infinite' : 'none',
-		overflow: 'hidden',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center'
-	};
-
-	const buttonStyle = {
-		position: 'relative',
-		width: btnWidth,
-		height: btnHeight,
-		padding: '0.75rem',
-		backgroundColor: 'transparent',
-		border: '1px solid black',
-		color: hover ? 'white' : 'black',
-		transition: '0s cubic-bezier(.11,.31,.92,.05)'
-	};
-
-	const colorBoxStyle = {
-		position: 'absolute',
-		width: '150%',
-		height: '150%',
-		zIndex: '-2',
-		transformOrigin: 'center',
-		transform: hover ? 'translate(0%,0%) scale(2) skew(0deg)' : 'translate(0%,400%) scale(2) skew(45deg)',
-		borderRadius: '0',
-		transition: '0s transform ease-in-out',
-		background:
-			'radial-gradient(circle, rgba(35,104,184,1) 0%, rgba(104,207,189,0.9906163148853291) 48%, rgba(61,53,209,1) 100%)',
-		opacity: '100%',
-		animation: hover ? 'rotate 5s cubic-bezier(0,.09,1,-0.09) 0s infinite alternate-reverse' : 'none'
-	};
-
-	const whiteBoxStyle = {
-		position: 'absolute',
-		top: '0',
-		left: '0',
-		width: '100%',
-		height: '100%',
-		backgroundColor: 'white',
-		zIndex: '-3'
-	};
-
 	return (
-		<div style={staticContainerStyle} onMouseOver={() => setHover(true)} onMouseLeave={() => setHover(false)}>
-			<div style={floatingContainerStyle} ref={floatRef}>
+		<div
+			style={getStaticContainerStyle(props.style)}
+			onMouseOver={() => setHover(true)}
+			onMouseLeave={() => setHover(false)}
+		>
+			<div style={getFloatingContainerStyle(props.style, { hover, pressed })} ref={floatRef}>
 				<button
-					style={{ ...buttonStyle, ...props.style }}
+					style={{ ...getButtonStyle(props.style, hover), ...props.style }}
 					onMouseDown={() => setPressed(true)}
 					onMouseUp={(val) => {
 						props.onClick(val);
@@ -81,7 +31,7 @@ const ColorButton = (props) => {
 				>
 					{props.text}
 				</button>
-				<div style={colorBoxStyle} />
+				<div style={getColorBoxStyle(hover)} />
 				<div style={whiteBoxStyle} />
 			</div>
 		</div>
---
+++ client/src/styling/colorButton.js
@@ -0,0 +1,76 @@
+export const BUTTON_BOUNCE_PX = 8;
+
+const getButtonSizes = (style) => {
+	return {
+		width: style && style.width ? style.width : 'auto',
+		height: style && style.height ? style.height : 'auto'
+	};
+};
+
+export const getStaticContainerStyle = (style) => {
+	return {
+		position: 'relative',
+		height: style && style.height ? `${parseInt(style.height) * 1.8}rem` : 'auto',
+		display: 'flex',
+		alignItems: 'center',
+		margin: '0.5rem'
+	};
+};
+
+export const getFloatingContainerStyle = (style, state) => {
+	const { width, height } = getButtonSizes(style);
+	const { pressed, hover } = state;
+	return {
+		position: 'relative',
+		width,
+		height,
+		top: pressed ? `${BUTTON_BOUNCE_PX}px` : hover ? `-${BUTTON_BOUNCE_PX}px` : '0',
+		transition: 'top 0.2s ease-in-out',
+		animation: hover ? 'float 1.4s ease-in-out 0.4s alternate infinite' : 'none',
+		overflow: 'hidden',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center'
+	};
+};
+
+export const getButtonStyle = (style, hover) => {
+	const { width, height } = getButtonSizes(style);
+	return {
+		position: 'relative',
+		width,
+		height,
+		padding: '0.75rem',
+		backgroundColor: 'transparent',
+		border: '1px solid black',
+		color: hover ? 'white' : 'black',
+		transition: '0s cubic-bezier(.11,.31,.92,.05)'
+	};
+};
+
+export const getColorBoxStyle = (hover) => {
+	return {
+		position: 'absolute',
+		width: '150%',
+		height: '150%',
+		zIndex: '-2',
+		transformOrigin: 'center',
+		transform: hover ? 'translate(0%,0%) scale(2) skew(0deg)' : 'translate(0%,400%) scale(2) skew(45deg)',
+		borderRadius: '0',
+		transition: '0s transform ease-in-out',
+		background:
+			'radial-gradient(circle, rgba(35,104,184,1) 0%, rgba(104,207,189,0.9906163148853291) 48%, rgba(61,53,209,1) 100%)',
+		opacity: '100%',
+		animation: hover ? 'rotate 5s cubic-bezier(0,.09,1,-0.09) 0s infinite alternate-reverse' : 'none'
+	};
+};
+
+export const whiteBoxStyle = {
+	position: 'absolute',
+	top: '0',
+	left: '0',
+	width: '100%',
+	height: '100%',
+	backgroundColor: 'white',
+	zIndex: '-3'
+};
