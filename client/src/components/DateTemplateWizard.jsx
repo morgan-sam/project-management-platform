9ceec588d7352a59dbf9198dcd887d7282cd22a9
Sam <samuel_morgan_@outlook.com>
2020-05-17 17:50:29 +0100

    DateTemplateWizard date&sequence state

--- client/src/components/DateTemplateWizard.jsx
+++ client/src/components/DateTemplateWizard.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import BatchNewTasks from 'components/BatchNewTasks';
+import DateSelect from 'components/DateSelect';
+import Checkbox from 'components/Checkbox';
+import { getDayFromTodayAsISO } from 'data/dates';
+import { parseISOToDateObj } from 'processing/parseDates';
 
 import { cancelButtonStyle, containerStyle } from 'styling/batchNewTasks';
+import { useState } from 'react';
 
 const DateTemplateWizard = (props) => {
 	const { setScreen } = props;
+	const [ date, setDate ] = useState(parseISOToDateObj(getDayFromTodayAsISO()));
+	const [ forward, setForwards ] = useState(true);
+
+	const sequenceContainer = {
+		display: 'grid',
+		gridTemplateColumns: 'repeat(2, 1fr)',
+		gridTemplateRows: 'repeat(2, 1fr)',
+		gridGap: '1rem',
+		padding: '1rem'
+	};
 
 	return (
 		<div
@@ -14,6 +28,20 @@ const DateTemplateWizard = (props) => {
 				width: '20rem'
 			}}
 		>
+			<div>Initial Date:</div>
+			<DateSelect date={date} setDate={setDate} />
+			<div>Sequence:</div>
+
+			<div style={sequenceContainer}>
+				<div style={{ gridArea: '1 / 1 / 2 / 2' }}>Forward:</div>
+				<Checkbox style={{ gridArea: '1 / 2 / 2 / 3' }} default={forward} onChange={() => setForwards(true)} />
+				<div style={{ gridArea: ' 2 / 1 / 3 / 2' }}>Backwards:</div>
+				<Checkbox
+					style={{ gridArea: ' 2 / 2 / 3 / 3' }}
+					default={!forward}
+					onChange={() => setForwards(false)}
+				/>
+			</div>
 			<button style={cancelButtonStyle} onClick={() => setScreen('main')}>
 				×
 			</button>
