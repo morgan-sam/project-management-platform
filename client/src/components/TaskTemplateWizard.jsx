096f15b531f439f05888480a42a64a2b14369a05
Sam <samuel_morgan_@outlook.com>
2020-05-18 09:31:11 +0100

    TaskWizard uses 2x2 grid only for interface

--- client/src/components/TaskTemplateWizard.jsx
+++ client/src/components/TaskTemplateWizard.jsx
@@ -11,13 +11,6 @@ const TaskTemplateWizard = (props) => {
 	const [ digits, setDigits ] = useState(1);
 	const [ order, setOrder ] = useState('a');
 
-	const parentContainer = {
-		display: 'grid',
-		gridTemplateColumns: 'repeat(2, 1fr)',
-		gridTemplateRows: 'repeat(6, 1fr)',
-		padding: '3rem'
-	};
-
 	const categoryStyle = {
 		display: 'flex',
 		flexDirection: 'column',
@@ -26,6 +19,12 @@ const TaskTemplateWizard = (props) => {
 		padding: '1rem',
 		textAlign: 'center',
 		margin: '1.5rem 0rem'
+	};
+
+	const mainGridContainer = {
+		display: 'grid',
+		gridTemplateColumns: 'repeat(2, 1fr)',
+		gridTemplateRows: 'repeat(2, 1fr)'
 	};
 
 	const getDigitContainerStyle = (symbol) => {
@@ -37,33 +36,31 @@ const TaskTemplateWizard = (props) => {
 
 	const topContainer = {
 		...titleStyle,
-		...categoryStyle,
-		gridArea: '1 / 1 / 2 / 3'
+		...categoryStyle
 	};
 
 	const nameContainer = {
 		...categoryStyle,
-		gridArea: '2 / 1 / 4 / 2'
+		gridArea: '1 / 1 / 2 / 2'
 	};
 
 	const symbolContainer = {
 		...categoryStyle,
-		gridArea: '4 / 1 / 6 / 2'
+		gridArea: '2 / 1 / 3 / 2'
 	};
 
 	const digitsContainer = {
 		...categoryStyle,
 		...getDigitContainerStyle(symbol),
-		gridArea: '2 / 2 / 4 / 3'
+		gridArea: '1 / 2 / 2 / 3'
 	};
 
 	const orderContainer = {
 		...categoryStyle,
-		gridArea: '4 / 2 / 6 / 3'
+		gridArea: '2 / 2 / 3 / 3'
 	};
 
 	const bottomContainer = {
-		gridArea: '6 / 1 / 7 / 3',
 		padding: '2rem',
 		display: 'flex',
 		alignItems: 'center',
@@ -92,62 +89,64 @@ const TaskTemplateWizard = (props) => {
 	};
 
 	return (
-		<div style={{ ...containerStyle, ...parentContainer }}>
+		<div style={{ ...containerStyle }}>
 			<div style={topContainer}>Generate Task Template</div>
-			<div style={nameContainer}>
-				<div style={{ padding: '1.5rem', paddingTop: '0.4rem' }}>Text Name:</div>
-				<form style={{ padding: '1rem' }} onChange={(e) => setName(e.target.value)}>
-					<input type="text" name="name" value={name} />
-					<input disabled type="submit" value="Submit" style={{ display: 'none' }} />
-				</form>
-			</div>
-			<div style={symbolContainer}>
-				<div style={containerItemStyle}>Symbol:</div>
-				<div style={{ ...subContainer, ...containerItemStyle }}>
-					<div style={{ gridArea: '1 / 1 / 2 / 2' }}>Number:</div>
-					<Checkbox
-						style={{ gridArea: '1 / 2 / 2 / 3' }}
-						default={symbol === 'n'}
-						onChange={() => setSymbol('n')}
-					/>
-					<div style={{ gridArea: '2 / 1 / 3 / 2' }}>Letter:</div>
-					<Checkbox
-						style={{ gridArea: '2 / 2 / 3 / 3' }}
-						default={symbol === 'l'}
-						onChange={() => setSymbol('l')}
+			<div style={mainGridContainer}>
+				<div style={nameContainer}>
+					<div style={{ padding: '1.5rem', paddingTop: '0.4rem' }}>Text Name:</div>
+					<form style={{ padding: '1rem' }} onChange={(e) => setName(e.target.value)}>
+						<input type="text" name="name" value={name} />
+						<input disabled type="submit" value="Submit" style={{ display: 'none' }} />
+					</form>
+				</div>
+				<div style={symbolContainer}>
+					<div style={containerItemStyle}>Symbol:</div>
+					<div style={{ ...subContainer, ...containerItemStyle }}>
+						<div style={{ gridArea: '1 / 1 / 2 / 2' }}>Number:</div>
+						<Checkbox
+							style={{ gridArea: '1 / 2 / 2 / 3' }}
+							default={symbol === 'n'}
+							onChange={() => setSymbol('n')}
+						/>
+						<div style={{ gridArea: '2 / 1 / 3 / 2' }}>Letter:</div>
+						<Checkbox
+							style={{ gridArea: '2 / 2 / 3 / 3' }}
+							default={symbol === 'l'}
+							onChange={() => setSymbol('l')}
+						/>
+					</div>
+				</div>
+				<div style={digitsContainer}>
+					<div style={{ ...containerItemStyle, paddingTop: '0.3rem' }}>{`Digits:`}</div>
+					<Dropdown
+						className="dropdown"
+						default={digits}
+						style={{
+							alignItems: 'center',
+							zIndex: '9',
+							width: '2rem',
+							padding: '2rem'
+						}}
+						options={[ ...Array(11).keys() ].slice(1)}
+						onClick={(val) => setDigits(val)}
 					/>
 				</div>
-			</div>
-			<div style={digitsContainer}>
-				<div style={{ ...containerItemStyle, paddingTop: '0.3rem' }}>{`Digits:`}</div>
-				<Dropdown
-					className="dropdown"
-					default={digits}
-					style={{
-						alignItems: 'center',
-						zIndex: '9',
-						width: '2rem',
-						padding: '2rem'
-					}}
-					options={[ ...Array(11).keys() ].slice(1)}
-					onClick={(val) => setDigits(val)}
-				/>
-			</div>
-			<div style={orderContainer}>
-				<div style={containerItemStyle}>Order:</div>
-				<div style={{ ...subContainer, ...containerItemStyle }}>
-					<div style={{ gridArea: '1 / 1 / 2 / 2' }}>Ascending:</div>
-					<Checkbox
-						style={{ gridArea: '1 / 2 / 2 / 3' }}
-						default={order === 'a'}
-						onChange={() => setOrder('a')}
-					/>
-					<div style={{ gridArea: '2 / 1 / 3 / 2' }}>Descending:</div>
-					<Checkbox
-						style={{ gridArea: '2 / 2 / 3 / 3' }}
-						default={order === 'd'}
-						onChange={() => setOrder('d')}
-					/>
+				<div style={orderContainer}>
+					<div style={containerItemStyle}>Order:</div>
+					<div style={{ ...subContainer, ...containerItemStyle }}>
+						<div style={{ gridArea: '1 / 1 / 2 / 2' }}>Ascending:</div>
+						<Checkbox
+							style={{ gridArea: '1 / 2 / 2 / 3' }}
+							default={order === 'a'}
+							onChange={() => setOrder('a')}
+						/>
+						<div style={{ gridArea: '2 / 1 / 3 / 2' }}>Descending:</div>
+						<Checkbox
+							style={{ gridArea: '2 / 2 / 3 / 3' }}
+							default={order === 'd'}
+							onChange={() => setOrder('d')}
+						/>
+					</div>
 				</div>
 			</div>
 			<div style={bottomContainer}>
