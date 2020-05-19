ac0456456707815e401d85c91aea9aa25b26eacc
Sam <samuel_morgan_@outlook.com>
2020-05-19 18:40:49 +0100

    Create 100s of years test

--- client/src/processing/batchNewTasks.test.js
+++ client/src/processing/batchNewTasks.test.js
@@ -50,3 +50,66 @@ test('Check fortnights backward 4 times date template', () => {
 	const result = interpretDateTemplate(dateTemplate, 4);
 	expect(result).toStrictEqual(expected);
 });
+
+test('Check 100s of years', () => {
+	const dateTemplate = '${1/1/1020}+100yn';
+	const expected = [
+		{
+			day: 1,
+			month: 1,
+			year: 1020
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1120
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1220
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1320
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1420
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1520
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1620
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1720
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1820
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 1920
+		},
+		{
+			day: 1,
+			month: 1,
+			year: 2020
+		}
+	];
+	const result = interpretDateTemplate(dateTemplate, 11);
+	expect(result).toStrictEqual(expected);
+});
