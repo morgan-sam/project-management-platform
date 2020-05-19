a9e51e85b0ec0119aa64156786f21f9eec520bbe
Sam <samuel_morgan_@outlook.com>
2020-05-19 18:35:51 +0100

    Backward fortnights test

--- client/src/processing/batchNewTasks.test.js
+++ client/src/processing/batchNewTasks.test.js
@@ -1,6 +1,6 @@
 import { interpretDateTemplate } from './batchNewTasks';
 
-test('Check for 3 day date template', () => {
+test('Check 3 sequential days date template', () => {
 	const dateTemplate = '${1/1/2020}+nd';
 	const expected = [
 		{
@@ -20,5 +20,33 @@ test('Check for 3 day date template', () => {
 		}
 	];
 	const result = interpretDateTemplate(dateTemplate, 3);
+	expect(result).toStrictEqual(expected);
+});
+
+test('Check fortnights backward 4 times date template', () => {
+	const dateTemplate = '${30/1/2080}-2nw';
+	const expected = [
+		{
+			day: 30,
+			month: 1,
+			year: 2080
+		},
+		{
+			day: 16,
+			month: 1,
+			year: 2080
+		},
+		{
+			day: 2,
+			month: 1,
+			year: 2080
+		},
+		{
+			day: 19,
+			month: 12,
+			year: 2079
+		}
+	];
+	const result = interpretDateTemplate(dateTemplate, 4);
 	expect(result).toStrictEqual(expected);
 });
