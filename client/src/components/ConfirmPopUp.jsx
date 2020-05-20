0f0d8952f56b9c36ec3c2b54b7b1ba83427c594b
Sam <samuel_morgan_@outlook.com>
2020-05-20 13:34:18 +0100

    Confirm popup styles sep file

--- client/src/components/ConfirmPopUp.jsx
+++ client/src/components/ConfirmPopUp.jsx
@@ -1,46 +1,10 @@
 import React, { useEffect } from 'react';
 import ColorButton from 'components/ColorButton';
+import { popUpContainerStyle, buttonContainerStyle, buttonStyle, messageContainerStyle } from 'styling/confirmPopUp';
 
 const ConfirmPopUp = (props) => {
 	const popUpCloseTimeMs = 400;
 	const { message, confirm, setPopUp, pressedKeys } = props;
-
-	const popUpContainerStyle = {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'center',
-		position: 'fixed',
-		top: '50vh',
-		left: '50vw',
-		height: '12rem',
-		width: '23rem',
-		border: '1px solid black',
-		transform: 'translate(-50%,-50%)',
-		backgroundColor: 'white',
-		zIndex: '10'
-	};
-
-	const buttonContainerStyle = {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-		margin: '1rem'
-	};
-
-	const buttonStyle = {
-		width: '5rem',
-		height: '2rem',
-		lineHeight: '0'
-	};
-
-	const messageContainerStyle = {
-		width: '75%',
-		textAlign: 'center',
-		lineHeight: '1.5rem',
-		margin: '2rem 0 0 0'
-	};
-
 	const closePopUp = () => setPopUp(null);
 
 	useEffect(
---
+++ client/src/styling/confirmPopUp.js
@@ -0,0 +1,35 @@
+export const popUpContainerStyle = {
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'center',
+	position: 'fixed',
+	top: '50vh',
+	left: '50vw',
+	height: '12rem',
+	width: '23rem',
+	border: '1px solid black',
+	transform: 'translate(-50%,-50%)',
+	backgroundColor: 'white',
+	zIndex: '10'
+};
+
+export const buttonContainerStyle = {
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	margin: '1rem'
+};
+
+export const buttonStyle = {
+	width: '5rem',
+	height: '2rem',
+	lineHeight: '0'
+};
+
+export const messageContainerStyle = {
+	width: '75%',
+	textAlign: 'center',
+	lineHeight: '1.5rem',
+	margin: '2rem 0 0 0'
+};
