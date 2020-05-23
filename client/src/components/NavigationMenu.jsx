37117e1a19bfc8e8ce8f8f3d965a552927bf97c4
Sam <samuel_morgan_@outlook.com>
2020-05-23 19:24:15 +0100

    NavMenu columns lining up correctly

--- client/src/components/NavigationMenu.jsx
+++ client/src/components/NavigationMenu.jsx
@@ -1,13 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import ThemeContext from 'context/ThemeContext';
 
 const NavigationMenu = (props) => {
 	const themeColor = useContext(ThemeContext);
 
+	const menus = [
+		{ name: 'File', sub: [ 'Batch New Tasks' ] },
+		{ name: 'Edit', sub: [ 'Select All', 'Mark Complete', 'Delete Selected' ] },
+		{ name: 'View', sub: [ 'Filter', 'New Task' ] }
+	];
+	const convertMenusToOpenObj = () => {
+		let obj = {};
+		for (let i = 0; i < menus.length; i++) {
+			obj[menus[i].name] = true;
+		}
+		return obj;
+	};
+	const [ menusOpen, setMenusOpen ] = useState(convertMenusToOpenObj());
+
 	const parentContainer = {
 		height: 'auto',
 		margin: '2rem 0',
 		display: 'flex',
+		justifyContent: 'left',
+		alignItems: 'top'
+	};
+
+	const boxStyle = {
+		textAlign: 'center',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		border: '1px solid black',
+		height: '2rem',
+		width: '4rem',
+		userSelect: 'none',
+		cursor: 'pointer',
+		fontSize: '0.75rem'
+	};
+
+	const mainMenuContainer = {
+		display: 'flex',
+		flexDirection: 'column',
 		justifyContent: 'left',
 		alignItems: 'center'
 	};
@@ -15,15 +49,9 @@ const NavigationMenu = (props) => {
 	const singleMenuBox = (box) => {
 		return (
 			<div
-				style={{
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					border: '1px solid black',
-					height: '2rem',
-					width: '4rem',
-					userSelect: 'none',
-					cursor: 'pointer'
+				style={boxStyle}
+				onClick={() => {
+					//
 				}}
 			>
 				{box}
@@ -31,10 +59,19 @@ const NavigationMenu = (props) => {
 		);
 	};
 
-	const boxes = [ 'File', 'Edit', 'View' ];
+	const multipleBoxes = (boxes) => {
+		return <div style={mainMenuContainer}>{boxes.map((el) => singleMenuBox(el))}</div>;
+	};
 
 	const mainRowOfBoxes = () => {
-		return boxes.map((el) => singleMenuBox(el));
+		return menus.map((el) => {
+			const { name, sub } = el;
+			console.log(name);
+			console.log(menusOpen);
+			console.log(menusOpen[name]);
+			if (menusOpen[name]) return multipleBoxes([ name, ...sub ]);
+			else return singleMenuBox(name);
+		});
 	};
 
 	return <div style={parentContainer}>{mainRowOfBoxes()}</div>;
