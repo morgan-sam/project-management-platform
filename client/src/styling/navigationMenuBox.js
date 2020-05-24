41e7f34c3b1c4d9e0c7a8fb4cae6a407887bb4f0
Sam <samuel_morgan_@outlook.com>
2020-05-24 21:18:03 +0100

    getMidBoxStyle returns border instead of null

--- client/src/styling/navigationMenuBox.js
+++ client/src/styling/navigationMenuBox.js
@@ -14,5 +14,5 @@ const getMidBoxStyle = (menuPos, menusOpen) => {
 	if (menuPos[1] === 0 && menuPos.length === 2) return { borderTop: 'none' };
 	else if (menuPos[menuPos.length - 1] === 0 && menuPos[menuPos.length - 2] === 0) return { borderTop: 'none' };
 	else if (menuPos[menuPos.length - 1] !== 0 && menuPos.length > 1) return { borderTop: 'none' };
-	else return null;
+	else return { borderTop: '1px solid black' };
 };
