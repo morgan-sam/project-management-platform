6d4a41a6b25c28f0332f646084c37c0fb67a799f
Sam <samuel_morgan_@outlook.com>
2020-05-24 21:17:27 +0100

    getMidBoxStyle fn

--- client/src/styling/navigationMenuBox.js
+++ client/src/styling/navigationMenuBox.js
@@ -1,11 +1,18 @@
 export const getIndividualBoxStyle = (menuPos, menusOpen) => {
 	let individualStyle;
-	const boxStyleFunctions = [ getTopBoxStyle ];
-	boxStyleFunctions.forEach((func) => (individualStyle = func(menuPos, menusOpen)));
+	const boxStyleFunctions = [ getTopBoxStyle, getMidBoxStyle ];
+	boxStyleFunctions.forEach((func) => (individualStyle = { ...individualStyle, ...func(menuPos, menusOpen) }));
 	return individualStyle;
 };
 
 const getTopBoxStyle = (menuPos, menusOpen) => {
 	if (menuPos[0] !== 0 && menuPos.length === 1) return { borderLeft: 'none' };
 	else return { borderLeft: '1px solid black' };
+};
+
+const getMidBoxStyle = (menuPos, menusOpen) => {
+	if (menuPos[1] === 0 && menuPos.length === 2) return { borderTop: 'none' };
+	else if (menuPos[menuPos.length - 1] === 0 && menuPos[menuPos.length - 2] === 0) return { borderTop: 'none' };
+	else if (menuPos[menuPos.length - 1] !== 0 && menuPos.length > 1) return { borderTop: 'none' };
+	else return null;
 };
