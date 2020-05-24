cab82f9dec115e903e24787eb1de6b9a594a1229
Sam <samuel_morgan_@outlook.com>
2020-05-24 19:45:59 +0100

    Iterates over array of functions

--- client/src/styling/navigationMenuBox.js
+++ client/src/styling/navigationMenuBox.js
@@ -1,10 +1,11 @@
 export const getIndividualBoxStyle = (menuPos, menusOpen) => {
 	let individualStyle;
-	individualStyle = getTopBoxStyle(menuPos);
+	const boxStyleFunctions = [ getTopBoxStyle ];
+	boxStyleFunctions.forEach((func) => (individualStyle = func(menuPos, menusOpen)));
 	return individualStyle;
 };
 
-const getTopBoxStyle = (menuPos) => {
+const getTopBoxStyle = (menuPos, menusOpen) => {
 	if (menuPos[0] !== 0 && menuPos.length === 1) return { borderLeft: 'none' };
 	else return { borderLeft: '1px solid black' };
 };
