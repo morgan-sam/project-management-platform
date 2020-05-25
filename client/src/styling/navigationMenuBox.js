abbfcdd407fa04451100955065df339474096a34
Sam <samuel_morgan_@outlook.com>
2020-05-25 10:27:24 +0100

    Removed all border modifications

--- client/src/styling/navigationMenuBox.js
+++ client/src/styling/navigationMenuBox.js
@@ -2,22 +2,9 @@ import { BOX_WIDTH_REM, BOX_HEIGHT_REM } from 'styling/navigationMenu';
 
 export const getIndividualBoxStyle = (menuPos, menusOpen) => {
 	let individualStyle;
-	const boxStyleFunctions = [ getTopBoxStyle, getMidBoxStyle, getBoxPosition ];
+	const boxStyleFunctions = [ getBoxPosition ];
 	boxStyleFunctions.forEach((func) => (individualStyle = { ...individualStyle, ...func(menuPos, menusOpen) }));
 	return individualStyle;
-};
-
-///STYLE///
-const getTopBoxStyle = (menuPos, menusOpen) => {
-	if (menuPos[0] !== 0 && menuPos.length === 1) return { borderLeft: 'none' };
-	else return { borderLeft: '1px solid black' };
-};
-
-const getMidBoxStyle = (menuPos, menusOpen) => {
-	if (menuPos[1] === 0 && menuPos.length === 2) return { borderTop: 'none' };
-	else if (menuPos[menuPos.length - 1] === 0 && menuPos[menuPos.length - 2] === 0) return { borderTop: 'none' };
-	else if (menuPos[menuPos.length - 1] !== 0 && menuPos.length > 1) return { borderTop: 'none' };
-	else return { borderTop: '1px solid black' };
 };
 
 ///POSITION///
