7d45efda836635fe04018e369c49f94a876d7acf
Sam <samuel_morgan_@outlook.com>
2020-05-27 21:39:44 +0100

    Working sortObjListByTask fn

--- client/src/processing/sortList.js
+++ client/src/processing/sortList.js
@@ -1,6 +1,7 @@
 export const sortList = (options, taskList) => {
 	const { sortOptions } = options;
 	if (taskList.length === 0) return [];
+	if (sortOptions.type === 'task') return sortObjListByTask(taskList, sortOptions.reversed);
 	if (sortOptions.type !== 'selected') return sortListByData(sortOptions, taskList);
 	else return sortListBySelected(options, taskList);
 };
@@ -39,7 +40,58 @@ const sortObjListAlphabetically = (list, params) => {
 	return list.sort((a, b) => (Object.values(a)[index] < Object.values(b)[index] ? -1 * reversed : 1 * reversed));
 };
 
-const sortObjListByTask = (list, params) => {
-	const { reversed } = params;
-	return list.sort((a, b) => (a.task < b.task ? -1 * reversed : 1 * reversed));
+const sortObjListByTask = (list, reversed) => {
+	const tasks = list.slice().map((el) => el.task);
+	const sortedTasks = getSortedMixedStringIndices(tasks);
+	const newList = sortedTasks.map((task) => {
+		const index = list.findIndex((x) => x.task === task);
+		return list[index];
+	});
+	if (reversed) return newList.slice().reverse();
+	else return newList;
+};
+
+const longestArrayString = (arr) => arr.reduce((a, b) => (a.length > b.length ? a : b));
+const numSort = (arr) => arr.slice().sort((a, b) => a - b);
+
+const consistentValues = (values) => {
+	for (let i = 0; i < values.length; i++) {
+		if (!isNaN(parseInt(values[i][0]))) values[i].unshift(null);
+		if (isNaN(parseInt(values[i][values[i].length - 1]))) values[i].push(Number.MAX_SAFE_INTEGER);
+	}
+	return values;
+};
+
+const getSortedMixedStringIndices = (arr) => {
+	let values = arr.map((el) =>
+		el.match(/([^0-9]+)|([0-9]+)/g).map((val) => (!isNaN(parseInt(val)) ? parseInt(val) : val))
+	);
+	values = consistentValues(values);
+	const rounds = longestArrayString(values).length;
+	let orders = arr.map(() => '');
+	for (let i = 0; i < rounds; i++) {
+		const toCompare = values.map((el) => el[i]);
+		const sorted = !isNaN(parseInt(toCompare[0])) ? numSort(toCompare.slice()) : toCompare.slice().sort();
+		const indices = toCompare.slice().map((el) => sorted.indexOf(el));
+		const digits = Math.max(...indices).toString().length;
+		indices.forEach((el, a) => {
+			if (typeof el === 'number') {
+				const elDigits = el.toString().length;
+				orders[a] += `${Array(digits - elDigits).fill('0').join('')}${el}`;
+			} else {
+				orders[a] += el;
+			}
+		});
+	}
+	orders = orders.map((el) => parseInt(`1${el}`));
+	const sortedOrders = numSort(orders.slice());
+	let indices = [];
+	for (let i = 0; i < orders.length; i++) {
+		const index = orders.indexOf(sortedOrders[i]);
+		indices.push(index);
+		orders[index] = null;
+	}
+	let newArray = arr.slice().map(() => null);
+	for (let i = 0; i < arr.length; i++) newArray[i] = arr[indices[i]];
+	return newArray;
 };
