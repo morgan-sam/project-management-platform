e7546bd51bb39c907fb3c69df0eaf5e88cf201f9
Sam <samuel_morgan_@outlook.com>
2020-06-15 19:54:11 +0100

    ambientBackground styles sep file

--- client/src/components/AmbientBackground.jsx
+++ client/src/components/AmbientBackground.jsx
@@ -1,31 +1,12 @@
 import React, { useContext } from 'react';
 import ThemeContext from 'context/ThemeContext';
+import { shapeStyle, parentContainer } from 'styling/ambientBackground';
 import { colorToWhiteArray } from 'processing/processColors';
 
 const AmbientBackground = (props) => {
 	const NUM_OF_SHAPES = 30;
 	const themeColor = useContext(ThemeContext);
 	const colors = colorToWhiteArray(themeColor, NUM_OF_SHAPES);
-
-	const shapeStyle = {
-		position: 'absolute',
-		display: 'block',
-		height: '100rem',
-		width: '40rem',
-		borderRadius: '100%',
-		animation: 'spiral 30s ease-in-out infinite alternate',
-		opacity: '0'
-	};
-
-	const parentContainer = {
-		position: 'fixed',
-		top: '0',
-		left: '0',
-		height: '100vh',
-		width: '100vw',
-		zIndex: '-1',
-		overflow: 'hidden'
-	};
 
 	const getBackgroundShapes = () => {
 		return [ ...Array(NUM_OF_SHAPES).keys() ].map((i) => (
---
+++ client/src/styling/ambientBackground.js
@@ -0,0 +1,19 @@
+export const shapeStyle = {
+	position: 'absolute',
+	display: 'block',
+	height: '100rem',
+	width: '40rem',
+	borderRadius: '100%',
+	animation: 'spiral 30s ease-in-out infinite alternate',
+	opacity: '0'
+};
+
+export const parentContainer = {
+	position: 'fixed',
+	top: '0',
+	left: '0',
+	height: '100vh',
+	width: '100vw',
+	zIndex: '-1',
+	overflow: 'hidden'
+};
