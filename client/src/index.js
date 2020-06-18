284d11ce3ddfd02f5406b09ddf7ae4777bf98286
Sam <samuel_morgan_@outlook.com>
2020-06-18 18:04:20 +0100

    table.css

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import 'css/reset.css';
 import 'css/index.css';
 import 'css/animations.css';
 import 'css/background.css';
+import 'css/table.css';
 import App from 'components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
