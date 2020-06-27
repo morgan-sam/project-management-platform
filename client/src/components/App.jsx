71c8c4aa40e74f6af066291a41e706b6573f5547
Sam <samuel_morgan_@outlook.com>
2020-06-27 19:01:37 +0100

    Login screen

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import MainScreen from 'components/MainScreen';
+import InputFormWithLabel from 'components/InputFormWithLabel';
+import ColorButton from 'components/ColorButton';
+import { accountScreenStyle, accountEntryBox } from 'styling/accountEntry';
 
 const App = () => {
-	return <MainScreen />;
+	const [ currentUser, setCurrentUser ] = useState();
+
+	if (currentUser === 'manager') return <MainScreen />;
+	if (currentUser === 'worker') return <MainScreen />;
+	return (
+		<div style={accountScreenStyle}>
+			<div style={accountEntryBox} className="accountEntryBox">
+				<InputFormWithLabel label={'Email'} />
+				<InputFormWithLabel label={'Password'} />
+				<ColorButton
+					style={{
+						fontSize: '1.8rem',
+						fontWeight: '600',
+						padding: '0.25rem 0.5rem',
+						letterSpacing: '0.04em'
+					}}
+					color={'#aaa'}
+					text={'Log In'}
+					onClick={() => null}
+				>
+					Log In
+				</ColorButton>
+			</div>
+		</div>
+	);
 };
 
 export default App;
---
+++ client/src/styling/accountEntry.js
@@ -0,0 +1,30 @@
+export const accountScreenStyle = {
+	width: '100vw',
+	height: '100vh',
+	position: 'absolute',
+	top: '0',
+	left: '0',
+	display: 'flex',
+	justifyContent: 'center',
+	alignItems: 'center'
+};
+
+export const accountEntryBox = {
+	width: 'fit-content',
+	height: '10rem',
+	padding: '2rem',
+	background: 'linear-gradient(205deg, rgba(255,255,255,1) 0%, rgba(173, 216, 230,1) 0%, rgba(255,0,0,0) 100%)',
+	border: '2px solid black',
+	borderRadius: '1rem',
+	fontSize: '1.7rem',
+	color: 'black',
+	WebkitTextFillColor: 'white',
+	WebkitTextStrokeWidth: '1px',
+	WebkitTextStrokeColor: 'black',
+	fontWeight: 'bold',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+	letterSpacing: '0.07em'
+};
