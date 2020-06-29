260537560e87dba1ff36c1294b5e3033a64226e8
Sam <samuel_morgan_@outlook.com>
2020-06-29 18:39:02 +0100

    Fixed some mistakes in text

--- client/src/data/createAccountText.js
+++ client/src/data/createAccountText.js
@@ -1,15 +1,12 @@
 export const createAccountText = [
 	[
-		'Welcome to PMP. This setup will create a new account to manage your team with.',
+		'Welcome to PMP. This setup will create a new group of accounts for your team.',
 		'If you are a team member, please contact your manager for your login details.'
 	],
-	[
-		'Firstly we will create the account of the team manager.',
-		'Please enter an email and password for the team manager.'
-	],
+	[ 'Firstly create the account of the team manager.', 'Please enter an email and password for the team manager.' ],
 	[
 		'Now create the account of each team member.',
-		'Please enter an email and password each team member.',
+		'Please enter an email and password for each team member.',
 		'(Note: members can be added/removed later)'
 	]
 ];
