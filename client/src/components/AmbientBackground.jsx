b220a99c7c1ace503d1464a7a0b70a25338ad673
Sam <samuel_morgan_@outlook.com>
2020-11-09 20:15:05 +0000

    AmbientBackground styled

--- client/src/components/AmbientBackground.jsx
+++ client/src/components/AmbientBackground.jsx
@@ -1,29 +1,28 @@
-import React, { useContext } from 'react';
-import ThemeContext from 'context/ThemeContext';
-import { shapeStyle, parentContainer } from 'styling/ambientBackground';
-import { colorToWhiteArray } from 'processing/colors';
+import React, { useContext } from "react";
+import ThemeContext from "context/ThemeContext";
+import { Shape, ShapeContainer } from "styling/ambientBackground";
+import { colorToWhiteArray } from "processing/colors";
 
 const AmbientBackground = (props) => {
-	const NUM_OF_SHAPES = 30;
-	const themeColor = useContext(ThemeContext);
-	const colors = colorToWhiteArray(themeColor, NUM_OF_SHAPES);
+  const NUM_OF_SHAPES = 30;
+  const themeColor = useContext(ThemeContext);
+  const colors = colorToWhiteArray(themeColor, NUM_OF_SHAPES);
 
-	const getBackgroundShapes = () => {
-		return [ ...Array(NUM_OF_SHAPES).keys() ].map((i) => (
-			<div
-				key={i}
-				style={{
-					...shapeStyle,
-					right: `${i}rem`,
-					top: `${i}rem`,
-					animationDelay: `${i / 10}s`,
-					backgroundColor: colors[i]
-				}}
-			/>
-		));
-	};
+  const getBackgroundShapes = () => {
+    return [...Array(NUM_OF_SHAPES).keys()].map((i) => (
+      <Shape
+        key={i}
+        style={{
+          right: `${i}rem`,
+          top: `${i}rem`,
+          animationDelay: `${i / 10}s`,
+          backgroundColor: colors[i],
+        }}
+      />
+    ));
+  };
 
-	return <div style={parentContainer}>{getBackgroundShapes()}</div>;
+  return <ShapeContainer>{getBackgroundShapes()}</ShapeContainer>;
 };
 
 export default AmbientBackground;
--- client/src/styling/ambientBackground.js
+++ client/src/styling/ambientBackground.js
@@ -1,19 +1,21 @@
-export const shapeStyle = {
-	position: 'absolute',
-	display: 'block',
-	height: '100rem',
-	width: '40rem',
-	borderRadius: '100%',
-	animation: 'spiral 30s ease-in-out infinite alternate',
-	opacity: '0'
-};
+import styled from "@emotion/styled";
 
-export const parentContainer = {
-	position: 'fixed',
-	top: '0',
-	left: '0',
-	height: '100vh',
-	width: '100vw',
-	zIndex: '-1',
-	overflow: 'hidden'
-};
+export const Shape = styled.div`
+  position: absolute;
+  display: block;
+  height: 100rem;
+  width: 40rem;
+  border-radius: 100%;
+  animation: spiral 30s ease-in-out infinite alternate;
+  opacity: 0;
+`;
+
+export const ShapeContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+  overflow: hidden;
+`;
