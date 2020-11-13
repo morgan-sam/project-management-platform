5e9399e6c340c7c0dafc678908c919ca250730ee
Sam <samuel_morgan_@outlook.com>
2020-11-13 22:50:27 +0000

    Created form for CreateAccountPageTwo

--- client/src/components/Screens/CreateAccountScreen/CreateAccountPageTwo.jsx
+++ client/src/components/Screens/CreateAccountScreen/CreateAccountPageTwo.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+/** @jsx jsx */
+import React, { useState } from 'react';
 import { Instruction, InstructionsContainer } from 'styling/createAccountStyle';
+import { css, jsx } from '@emotion/react';
 
 const CreateAccountPageTwo = (props) => {
-    const { managerDetails, setManagerDetails } = props;
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const CreateAccountFormStyle = css`
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        margin: 1rem;
+        & > * {
+            margin: 0.5rem;
+        }
+        & > label {
+            color: white;
+        }
+    `;
+
+    const handleEmailChange = (e) => setEmail(e.target.value);
+    const handlePasswordChange = (e) => setPassword(e.target.value);
+
     return (
         <div>
             <InstructionsContainer>
@@ -13,6 +33,16 @@ const CreateAccountPageTwo = (props) => {
                     Please enter an email and password for the team manager.
                 </Instruction>
             </InstructionsContainer>
+            <form css={CreateAccountFormStyle}>
+                <label>Email</label>
+                <input type="text" value={email} onChange={handleEmailChange} />
+                <label>Password</label>
+                <input
+                    type="text"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </form>
         </div>
     );
 };
