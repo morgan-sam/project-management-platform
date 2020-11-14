927ebf124ede35fbfd488bc422c8d31c4dfe0b7b
Sam <samuel_morgan_@outlook.com>
2020-11-14 16:56:07 +0000

    WizardButton uses emotion styles

--- client/src/components/WizardButton.jsx
+++ client/src/components/WizardButton.jsx
@@ -1,33 +1,34 @@
 import React, { useState, useContext } from 'react';
 import ThemeContext from 'context/ThemeContext';
+import styled from '@emotion/styled';
 
 const WizardButton = (props) => {
     const [hover, setHover] = useState(false);
     const themeColor = useContext(ThemeContext);
 
-    const wizardBtnStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '1.3rem',
-        height: '1.3rem',
-        border: '1px solid black',
-        userSelect: 'none',
-        color: hover ? 'white' : 'black',
-        backgroundColor: hover ? themeColor : 'white'
-    };
+    const WizardBtn = styled.button`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 1.3rem;
+        height: 1.3rem;
+        border: 1px solid black;
+        user-select: none;
+        cursor: pointer;
+        color: ${hover ? 'white' : 'black'};
+        background-color: ${hover ? themeColor : 'white'};
+    `;
 
     return (
-        <div
+        <WizardBtn
             onClick={(val) => props.onClick(val)}
             onMouseOver={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
-            style={wizardBtnStyle}
         >
             <span role="img" aria-label="lightning">
                 ⚡
             </span>
-        </div>
+        </WizardBtn>
     );
 };
 
