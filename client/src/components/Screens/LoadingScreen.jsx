b6c27d204ee73a62e9e1415e32cc948dfc113428
Sam <samuel_morgan_@outlook.com>
2020-11-22 13:24:15 +0000

    LoadingScreen all styled

--- client/src/components/Screens/LoadingScreen.jsx
+++ client/src/components/Screens/LoadingScreen.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
-export const loadingScreenStyle = {
-    position: 'absolute',
-    top: '0',
-    left: '0',
-    display: 'flex',
-    height: '100vh',
-    width: '100vw',
-    alignItems: 'center',
-    justifyContent: 'center'
-};
+import styled from '@emotion/styled';
 
-export const spinnerStyle = {
-    height: '3rem',
-    width: '3rem',
-    borderTop: '10px solid #4287f5',
-    borderRight: '10px solid #eee',
-    borderBottom: '10px solid #eee',
-    borderLeft: '10px solid #eee',
-    borderRadius: '100%',
-    animation: 'spin 0.75s cubic-bezier(.17,.67,.83,.67) infinite'
-};
+export const SpinnerContainer = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Spinner = styled.div`
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    height: 3rem;
+    width: 3rem;
+    border-top: 10px solid #4287f5;
+    border-right: 10px solid #eee;
+    border-bottom: 10px solid #eee;
+    border-left: 10px solid #eee;
+    border-radius: 100%;
+    animation: spin 0.75s cubic-bezier(0.17, 0.67, 0.83, 0.67) infinite;
+`;
 
 const LoadingScreen = () => {
     return (
-        <div style={loadingScreenStyle}>
-            <div style={spinnerStyle} />
-        </div>
+        <SpinnerContainer>
+            <Spinner />
+        </SpinnerContainer>
     );
 };
 export default LoadingScreen;
