ffd30c48fb73f33b28af0ea5d1ec5968aeef5bca
Sam <samuel_morgan_@outlook.com>
2020-12-03 17:01:49 +0000

    Removed obsolete break statements

--- client/src/components/ChangeValues/ChangeValuesInput.jsx
+++ client/src/components/ChangeValues/ChangeValuesInput.jsx
@@ -7,24 +7,19 @@ const ChangeValuesInput = (props) => {
         case 'task':
             // text input
             return <div>text input</div>;
-            break;
         case 'date':
         case 'deadline':
             // date input
             return <div>date input</div>;
-            break;
         case 'urgency':
             // integer input
             return <div>integer input</div>;
-            break;
         case 'teams':
             // array input
             return <div>array input</div>;
-            break;
         case 'completed':
             // boolean input
             return <div>boolean input</div>;
-            break;
         default:
             return <div>ERROR</div>;
     }
